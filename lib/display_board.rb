466fdbcad193c635f1d4164d4d700fdacef31696
Angela Bardysh <angelabardysh@gmail.com>
2016-09-10 01:17:10 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,19 +1,9 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
-def display_board
-  puts "   ""|""   ""|""   "
-  puts "-----------"
-  puts "   ""|""   ""|""   "
-  puts "-----------"
-  puts "   ""|""   ""|""   "
-end
-
-display_board
-
 def display_board(board)
-  puts "#{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts "#{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts "#{board[6]} | #{board[7]} | #{board[8]} "
+puts" #{board[0]} | #{board[1]} | #{board[2]} "
+puts"-----------"
+puts" #{board[3]} | #{board[4]} | #{board[5]} "
+puts"-----------"
+puts" #{board[6]} | #{board[7]} | #{board[8]} "
 end
