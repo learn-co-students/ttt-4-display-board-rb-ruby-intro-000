72de219b4c569ff0640fd9260470f50697e10526
John Erickson <ericksonjohn08@hotmail.com>
2016-09-16 19:09:13 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board(array_of_empty_strings)
-	puts "#{array_of_empty_strings[0]}  | #{array_of_empty_strings[1]} | #{array_of_empty_strings[2]} "
+def display_board(board)
+	puts " #{board[0]} | #{board[1]} | #{board[2]} "
 	puts "-----------"
-	puts "#{array_of_empty_strings[3]}  | #{array_of_empty_strings[4]} | #{array_of_empty_strings[5]} "
+	puts " #{board[3]} | #{board[4]} | #{board[5]} "
 	puts "-----------"
-	puts "#{array_of_empty_strings[6]}  | #{array_of_empty_strings[7]} | #{array_of_empty_strings[8]} "
+	puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-display_board([" ", " ", " ", " ", " ", " ", " ", " ", " "])
+display_board([" "," "," "," "," "," "," "," "," "])
