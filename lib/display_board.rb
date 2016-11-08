96dc374e8039cc7281c8e2fa19dd6980c85ae9d4
Betsy Deuman <deumanma@gmail.com>
2016-11-08 01:55:24 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,5 @@
 # Define display_board that accepts a board and prints
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  puts [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 def display_board(board)
   puts "   |   |   "
   puts "-----------"
