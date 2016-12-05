daeddc03a8b83ac406d18ed3a4300fdae0b5b984
Cameron Dunne <camdunne@gmail.com>
2016-12-05 04:00:15 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,20 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  i = 0
+  row = ""
+  while i<board.length
+    if i == board.length-1
+    	 row += " #{board[i]} "
+    	 puts row
+    elsif (i+1) % 3 == 0
+	 	row += " #{board[i]} "
+    	puts row
+    	row = ""
+      puts "-----------"
+    else
+      row += " #{board[i]} |"
+    end
+    i += 1
+  end
+end
