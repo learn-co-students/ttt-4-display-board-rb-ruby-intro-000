8f58c8f8bc9798b6ea6aec2e74f74a4d01c42819
Filip Zindovic <fzindovic@gmail.com>
2017-01-28 12:38:11 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,13 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+#require.relative (display_board)
+#board = [" "," "," "," "," "," "," "," "," "]
+#display_board(board)
+board = [" ", " "," "," "," "," "," "," "," "]
+def display_board(board)   
+  puts [" #{"X"} |"" #{"X"} |"" #{"X"} "]
+  puts "-----------" 
+  puts [" #{" "} |"" #{" "} |"" #{" "} "]
+  puts "-----------"
+  puts [" #{" "} |"" #{" "} |"" #{" "} "]
+end 
+display_board(board)
+
