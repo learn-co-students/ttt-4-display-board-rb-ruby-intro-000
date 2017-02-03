cf2a494c8397263517d7f5b371967d1e1c37da5d
Benton Wong <bentonwong@gmail.com>
2017-02-03 05:26:53 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board (pos)
+  puts " #{pos[0]} " + "|" + " #{pos[1]} " + "|" + " #{pos[2]} "
+  puts "-----------"
+  puts " #{pos[3]} " + "|" + " #{pos[4]} " + "|" + " #{pos[5]} "
+  puts "-----------"
+  puts " #{pos[6]} " + "|" + " #{pos[7]} " + "|" + " #{pos[8]} "
+end
