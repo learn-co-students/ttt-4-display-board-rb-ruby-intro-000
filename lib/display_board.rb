652455ba0ad482c0a7c4f7b08c04db3083574d0a
Kyle Andrews <kylemandrews@gmail.com>
2017-03-01 19:24:29 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,32 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(move = " ")
+  row = "-----------"
+  cell_one = " #{move[0]} " + "|" + " #{move[1]} " + "|" + " #{move[2]} "
+  cell_two = " #{move[3]} " + "|" + " #{move[4]} " + "|" + " #{move[5]} "
+  cell_three = " #{move[6]} " + "|" + " #{move[7]} " + "|" + " #{move[8]} "
+puts cell_one
+puts row
+puts cell_two
+puts row
+puts cell_three
+end
+
+board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
+display_board(board)
+
+board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+display_board(board)
+
+board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
+display_board(board)
+
+board = [" ", " ", " ", " ", " ", " ", "O", "O", "O"]
+display_board(board)
+
+board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
+display_board(board)
+
+board = [" ", " ", "O", " ", "O", " ", "O", " ", " "]
+display_board(board)
+
+board = [" ", "X", "X", " ", " ", "X", " ", "X", " "]
+display_board(board)
