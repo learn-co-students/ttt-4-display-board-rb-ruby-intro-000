3c6d1cfb00793471c111b097dd11255b69a7e28f
Jason Tseng <jason.f.tseng@gmail.com>
2017-04-17 01:39:32 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,15 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(cells = [" ", " ", " ", " ", " ", " ", " ", " ", " ",])
+
+  row1 = " #{cells[0]} | #{cells[1]} | #{cells[2]} "
+  row2 = "-----------"
+  row3 = " #{cells[3]} | #{cells[4]} | #{cells[5]} "
+  row4 = "-----------"
+  row5 = " #{cells[6]} | #{cells[7]} | #{cells[8]} "
+  puts row1
+  puts row2
+  puts row3
+  puts row4
+  puts row5
+end
