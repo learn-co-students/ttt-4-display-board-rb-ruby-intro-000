ebb6453159bea084032d5b320fa69ac0a66365ee
Paige Skinner <skinnerpa86@yahoo.com>
2017-05-30 15:41:48 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(zeboard)
+  puts " #{zeboard[0]} | #{zeboard[1]} | #{zeboard[2]} "
+  puts "-----------"
+  puts " #{zeboard[3]} | #{zeboard[4]} | #{zeboard[5]} "
+  puts "-----------"
+  puts " #{zeboard[6]} | #{zeboard[7]} | #{zeboard[8]} "
+end
+  zeboard = [" "," "," "," "," "," "," "," "]
+  display_board(zeboard)
+puts "turn 1"
+zeboard = [" "," "," "," ","x"," "," "," "]
+display_board(zeboard)
