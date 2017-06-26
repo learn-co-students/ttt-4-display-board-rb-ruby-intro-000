27a72dc9e75794b61aeaf6c60f9a7c2a731bb76d
Ibrahim  Mamdouh <i2771995@gmail.com>
2017-06-26 15:05:10 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+# Define a method display_board that prints a 3x3 Tic Tac Toe Board
+# board = [" "] * 9
+
+def display_board(board)
+  for i in (0...9).step(3)
+    puts " #{board[i]} | #{board[i+1]} | #{board[i+2]} "
+    puts '-----------' if i!=2
+  end
+end
+# board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+# display_board(board)
