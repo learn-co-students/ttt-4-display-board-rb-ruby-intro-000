19ffdce4da79d3783d9e449e6a5d7b6e65fecd3f
Di Livius <liviu.sirghea@yahoo.com>
2017-06-26 09:52:59 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,19 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board = [" "," "," "," "," "," "," "," "," "])
+  row_separator = "-----------"
+  element_separator = "|"
+
+  board.each_with_index do |element, index|
+    print " #{board[index]} "
+    temp_index = index+1
+    if (temp_index % 3 != 0)
+      print element_separator
+    else
+      puts ""
+      puts row_separator unless index == board.length-1
+    end
+  end
+end
+
+#display_board()
