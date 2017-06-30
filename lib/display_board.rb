a9bfc38c05d4657f4c336e5a289febab7e9a1489
Keith Kerr <kkerr123@gmail.com>
2017-06-30 03:31:55 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,36 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+
+def display_board (board = " ")
+  cell1 = " #{board[0]} "
+  cell2 = " #{board[1]} "
+  cell3 = " #{board[2]} "
+  cell4 = " #{board[3]} "
+  cell5 = " #{board[4]} "
+  cell6 = " #{board[5]} "
+  cell7 = " #{board[6]} "
+  cell8 = " #{board[7]} "
+  cell9 = " #{board[8]} "
+  devider = "|"
+  dash = "-----------"
+  print cell1
+  print devider
+  print cell2
+  print devider
+  puts cell3
+  puts dash
+  print cell4
+  print devider
+  print cell5
+  print devider
+  puts cell6
+  puts dash
+  print cell7
+  print devider
+  print cell8
+  print devider
+  puts cell9
+end
+
+display_board
