296314ff6c84ef52347c5f1b7a11f4c700634b1f
Jiening Chen <jieningjchen@gmail.com>
2017-07-15 21:04:54 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  pipe = "|"
+  lines = "-----------"
+  spaces = "   "
+
+  puts ' '"#{spaces = board[0]} #{pipe} #{spaces = board[1]} #{pipe} #{spaces = board[2]} "
+  puts "#{lines}"
+  puts ' '"#{spaces = board[3]} #{pipe} #{spaces = board[4]} #{pipe} #{spaces = board[5]} "
+  puts "#{lines}"
+  puts ' '"#{spaces = board[6]} #{pipe} #{spaces = board[7]} #{pipe} #{spaces = board[8]} "
+end
