1fc0043deb5df06a839264da52cd4e3107a3ea7e
Lonnie Poupard Jr <lonnie.d.poupard@gmail.com>
2017-08-03 03:19:31 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,16 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+board = [" "," "," "," "," "," "," "," "," "]
+
+def display_board(move)
+  row_1 = " #{move[0]} | #{move[1]} | #{move[2]} "
+  row_2 = "-----------"
+  row_3 = " #{move[3]} | #{move[4]} | #{move[5]} "
+  row_4 = "-----------"
+  row_5 = " #{move[6]} | #{move[7]} | #{move[8]} "
+  puts row_1
+  puts row_2
+  puts row_3
+  puts row_4
+  puts row_5
+end
