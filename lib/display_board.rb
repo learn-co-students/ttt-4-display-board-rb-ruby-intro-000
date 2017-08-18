c761426c466b2d0b89d512a29ecdd8980d25176e
Grant Regimbal <regimbal.grant@gmail.com>
2017-08-18 21:34:37 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board = [" "," "," "," "," "," "," "," "," "])
+  puts " #{board[0]} | #{board[1]} | #{board[2]} " #row 1
+  puts "-----------" #line 1
+  puts " #{board[3]} | #{board[4]} | #{board[5]} " #row 2
+  puts "-----------" #line 2
+  puts " #{board[6]} | #{board[7]} | #{board[8]} " #row 3
+
+end
