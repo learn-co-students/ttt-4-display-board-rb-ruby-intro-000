8bf127ad808ac3118333b8da533f823dafd4c23d
ashwinvidiyala <ashwinvidiyala@gmail.com>
2017-08-21 21:26:57 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,18 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+board = Array.new(9, "   ")
+
+def display_board(board)
+
+  dash = "-----------"
+
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts dash
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts dash
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  
+end
+
+display_board(board)
