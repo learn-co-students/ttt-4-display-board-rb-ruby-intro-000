5b443425b2e47e6e1abffc3ff6000933880d71ad
Thaddaeus Caldwell <tpcaldwell@gmail.com>
2017-09-19 18:43:09 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,7 +4,7 @@ board = [" ", " ", " ",
         " ", " ", " ",
         " ", " ", " "]
 def display_board(board)
-puts "board[0] | #{board[1]} | #{board[2]} "
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
 puts "-----------"
 puts " #{board[3]} | #{board[4]} | #{board[5]} "
 puts "-----------"
