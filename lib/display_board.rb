685c687d3400ff10c5b4d2ba5e54c5f98767b1c7
Jawann <jawann@gmail.com>
2017-10-22 01:41:46 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,8 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(game)
+  inside = "-" * 11
+  puts " #{game[0]} | #{game[1]} | #{game[2]} "
+  puts inside
+  puts " #{game[3]} | #{game[4]} | #{game[5]} "
+  puts inside
+  puts " #{game[6]} | #{game[7]} | #{game[8]} "
+end
