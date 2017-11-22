18e93b6d6f07660284dfb4e4df7aae09c7018521
Yassin Abo El Nour <yassinaboelnour@gmail.com>
2017-11-22 16:37:49 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,16 @@
+def display_board (board)
+
+  toprow = " #{board[0]} | #{board[1]} | #{board[2]} "
+  midrow = " #{board[3]} | #{board[4]} | #{board[5]} "
+  botrow = " #{board[6]} | #{board[7]} | #{board[8]} "
+  dash = "-----------"
+
+  puts toprow
+  puts dash
+  puts midrow
+  puts dash
+  puts botrow
+
+end
 # Define display_board that accepts a board and prints
 # out the current state.
