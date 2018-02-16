2ce9ee7ffa85ea53a5aa840fc9a96ec454c2031c
Gerardo Dueñas <geras998@gmail.com>
2018-02-16 01:01:18 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,13 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(board)
+  9.times do |i|
+    print " #{board[i]} "
+    if (i+1) % 3 == 0 and i < 8 then
+      puts ""
+      puts "-" * 11
+    end
+    print "|" if (i+1) % 3 != 0
+  end
+  puts ""
+end
