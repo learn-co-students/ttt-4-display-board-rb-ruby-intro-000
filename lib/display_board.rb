bb645e3d11ec31cb9169bb27ddbcacf9ac9df404
Nicholas Rekuch <rekuchn@gamil.com>
2018-05-14 19:25:52 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,16 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+board = [" "," "," "," "," "," "," "," "," "]
+
+
+def display_board(n)
+  puts " " + n[0] + " | " + n[1] + " | " + n[2] + " "
+  puts "-----------"
+  puts " " + n[3] + " | " + n[4] + " | " + n[5] + " "
+  puts "-----------"
+  puts " " + n[6] + " | " + n[7] + " | " + n[8] + " "
+
+end
+
+display_board(board)
