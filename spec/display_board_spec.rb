911fc2e709d2e50981920eb13e7418a1b4715067
bryanfong.nyc@gmail.com <bryanfong.nyc@gmail.com>
2018-06-14 13:32:31 +0000

    Done.

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -137,6 +137,36 @@ describe "#display_board in 'lib/display_board.rb" do
     end
 
     it 'prints an entire board full of Xs' do
+      board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
+
+      output = capture_puts{ display_board(board) }
+      rows = output.split("\n")
+
+      expect(rows[0]).to eq(" X | X | X ")
+      expect(rows[1]).to eq("-----------")
+      expect(rows[2]).to eq(" X | X | X ")
+      expect(rows[3]).to eq("-----------")
+      expect(rows[4]).to eq(" X | X | X ")
+    end
+
+    it 'prints an entire board full of Os' do
+      board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
+
+      output = capture_puts{ display_board(board) }
+      rows = output.split("\n")
+
+      expect(rows[0]).to eq(" O | O | O ")
+      expect(rows[1]).to eq("-----------")
+      expect(rows[2]).to eq(" O | O | O ")
+      expect(rows[3]).to eq("-----------")
+      expect(rows[4]).to eq(" O | O | O ")
+    end
+  end
+end
+
+      # How to Add a Test for board filled with 'X's. 
+      
+      
       # Should you want to write your own test for this situation,
       # read the following code and comments.
 
@@ -170,21 +200,7 @@ describe "#display_board in 'lib/display_board.rb" do
       # expect(rows[2]).to eq("   |   |   ")
       # expect(rows[3]).to eq("-----------")
       # expect(rows[4]).to eq("   |   |   ")
-
+      
       # *** Comment the line below by adding a # at the line start ***
-      expect(true).to be(true)
-    end
+      # *** expect(true).to be(true)
 
-    it 'prints an entire board full of Os' do
-      # Can you copy the syntax of the tests above to write a test for a board
-      # entirely filled with Os?
-
-      # Hint: You should be able to copy the code in the previous it example
-      # and make a few simple edits to convert the previous example to this
-      # example's situation.
-
-      # *** Comment the line below by adding a # at the line start ***
-      expect(true).to be(true)
-    end
-  end
-end
