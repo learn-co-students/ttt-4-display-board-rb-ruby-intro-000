ae873aacf4bf34b1d44749644e886ce36780a75d
Michael Shaw <michael.gene.shaw@gmail.com>
2018-07-16 20:54:14 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,17 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def row(row)
+  " #{row[0]} | #{row[1]} | #{row[2]} "
+end
+
+def divider
+  "-----------"
+end
+
+def display_board(board)
+  puts row(board[0, 3])
+  puts divider
+  puts row(board[3, 6])
+  puts divider
+  puts row(board[6, 9])
+end
