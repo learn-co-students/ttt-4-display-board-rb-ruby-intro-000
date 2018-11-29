92618155f38da9619cd12ec8cf10c4ee73788ffd
shivani brijmohan <sbrij001@yahoo.com>
2018-11-29 20:15:13 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,11 +7,11 @@
 #create a default board to output
 #use interpolation to take the board parameters and add it to the default board
 
-board = [" "," "," "," "," "," "," "," "," "]
+board = ["   ", "   ", "   ","   ","   ","   ","   ","   ","   "]
 def display_board(board)
-  puts  " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts  "-----------"
-  puts  " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts  "-----------"
-  puts  " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
