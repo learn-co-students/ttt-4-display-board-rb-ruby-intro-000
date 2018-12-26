4dc07e26f9cb6ae12aa7ca86f238f2e0ad9413d2
jovial-system-3966 <ramvasuthevan@gmail.com>
2018-12-26 06:39:24 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,16 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board(board)
+  puts generate_row(board[0..2])
+  puts "-"*11
+
+  puts generate_row(board[3..5])
+  puts "-"*11
+
+  puts generate_row(board[6..8])
+end
+
+def generate_row(row)
+  return " #{row[0]} | #{row[1]} | #{row[2]} "
+end
