c58b8e03626500a72d328a1b58ca486e4ec6e088
Sohaib Mubashir <sohaibmh@yahoo.com>
2019-03-31 12:48:12 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,19 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(array)
+  if array.empty?
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+else
+  puts " #{array[0]} | #{array[1]} | #{array[2]} "
+  puts "-----------"
+  puts " #{array[3]} | #{array[4]} | #{array[5]} "
+  puts "-----------"
+  puts " #{array[6]} | #{array[7]} | #{array[8]} "
+  end
+end
+
+moves = ["X", "O", " ", " ", "X", " ", " ", "O", " "]
+
+display_board(moves)
