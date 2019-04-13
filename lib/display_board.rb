67858ab18e06781951bbd67fa46b145e3b4f627b
Wathon Aung <wathonaun9@gmail.com>
2019-04-13 15:03:15 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,7 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(board_param = [" "," "," "," "," "," "," "," "," "])
+  puts " #{board_param[0]} | #{board_param[1]} | #{board_param[2]} "
+  puts "-----------"
+  puts " #{board_param[3]} | #{board_param[4]} | #{board_param[5]} "
+  puts "-----------"
+  puts " #{board_param[6]} | #{board_param[7]} | #{board_param[8]} "
+end
