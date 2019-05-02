53319f8256dbdb91219fbc94261973909b8cf986
Jason <me@httplife.com>
2019-05-02 05:57:38 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+board = [" "," "," "," "," "," "," "," "," "]
+
+def display_board(board)
+	puts " "+board[0].upcase+" "+"|"+" "+board[1].upcase+" "+"|"+" "+board[2].upcase+" "
+	puts "-----------"
+  puts " "+board[3].upcase+" "+"|"+" "+board[4].upcase+" "+"|"+" "+board[5].upcase+" "
+	puts "-----------"
+	puts " "+board[6].upcase+" "+"|"+" "+board[7].upcase+" "+"|"+" "+board[8].upcase+" "
+end
