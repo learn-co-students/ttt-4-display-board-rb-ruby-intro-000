dc24e87a5fd0fedf6d3067a25f10cbc0dba7206e
Amy Nguyen <amy.nguyen730@gmail.com>
2019-08-15 14:19:10 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,38 @@
 # Define display_board that accepts a board and prints
+def display_board(the_board)
+  puts " #{the_board[0]} | #{the_board[1]} | #{the_board[2]} "
+  puts "-----------"
+  puts " #{the_board[3]} | #{the_board[4]} | #{the_board[5]} "
+  puts "-----------"
+  puts " #{the_board[6]} | #{the_board[7]} | #{the_board[8]} "
+end
+
+board = ["", "", "", "", "", "", "", "", ""]
+display_board(board)
+
+
+puts "Turn 1 happened, X went in the middle"
+board = ["", "", "", "", "X", "", "", "", ""]
+display_board(board)
+
+puts "Turn 2 happened, X went in the middle"
+board = ["O", "", "", "", "", "", "", "", ""]
+display_board(board)
+
+puts "Turn 3 happened, X went in the middle"
+board = ["O", "", "", "", "X", "", "", "", ""]
 # out the current state.
+
+
+
+
+
+
+
+#def display_board(the_board)
+#  puts "   |   |   "
+#  puts "-----------"
+#  puts "   |   |   "
+#  puts "-----------"
+#  puts "   |   |   "
+#end
