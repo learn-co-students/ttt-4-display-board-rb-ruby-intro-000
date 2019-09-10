6b9fb90daca846a44a83938c85fc93756d3966d1
Arjun Nair <nair.arjun47@yahoo.com>
2019-09-10 21:23:06 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,16 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+
+
+def display_board(board) 
+  
+  pipes = "|"
+  rows = "-----------"
+
+  puts " #{board[0]} #{pipes} #{board[1]} #{pipes} #{board[2]} "
+  puts "#{rows}"
+  puts " #{board[3]} #{pipes} #{board[4]} #{pipes} #{board[5]} "
+  puts "#{rows}"
+  puts " #{board[6]} #{pipes} #{board[7]} #{pipes} #{board[8]} "
+end
