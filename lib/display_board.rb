c0f1a90bd29b039d8c207c9b4a5b11eb09844c4e
Aaron Gokani <aarongokani@hotmail.com>
2019-10-09 01:46:26 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board(tiles)
+  puts " #{tiles[0]} | #{tiles[1]} | #{tiles[2]} "
+  puts "-----------"
+  puts " #{tiles[3]} | #{tiles[4]} | #{tiles[5]} "
+  puts "-----------"
+  puts " #{tiles[6]} | #{tiles[7]} | #{tiles[8]} "
+end
