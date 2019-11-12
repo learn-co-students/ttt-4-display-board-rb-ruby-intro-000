b1d9064a4dc4e21c96e7f4ad3810ce7af6654a65
lior Shine <shinelior@gmail.com>
2019-11-12 21:51:31 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,16 @@
 # Define display_board that accepts a board and prints
-# out the current state.
+# out the current state.'
+#z= ['   0','|x |',' ','x ','|x |',' ',' ','|x |', '']
+# = [' 0 ','|x |',' ',]
+def display_board0(board)
+  puts  "#{board[0]} |  #{board[1]}|   #{board[2]}"
+
+  puts "-----------"
+  puts "#{board[3]}  | #{board[4]} |  #{board[5]}"
+
+  puts "-----------"
+  puts "#{board[6]}  | #{board[7]} |  #{board[8]}"
+  
+ 
+#print row[1]
+end
