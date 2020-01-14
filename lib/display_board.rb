9fc5ea91d1911c5ea48ab10da17138874c91d06d
William Lin <willlin1996@gmail.com>
2020-01-14 17:52:49 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,24 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-def display_board
- puts "   |   |   "
- puts "-----------"
- puts "   |   |   "
- puts "-----------"
- puts "   |   |   "
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
  end
- 
-   it 'prints an entire board full of Os' do
-      
-      board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
-      
-      output = capture_puts{ display_board(board) }
-      rows = output.split("\n")
-      
-       expect(rows[0]).to eq(" O | O | O ")
-       expect(rows[1]).to eq("-----------")
-       expect(rows[2]).to eq(" O | O | O ")
-       expect(rows[3]).to eq("-----------")
-       expect(rows[4]).to eq(" O | O | O ")
-       expect(true).to be(true)
-     end
