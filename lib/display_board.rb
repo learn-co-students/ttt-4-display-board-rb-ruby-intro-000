ce1384ce7326a7f80e86cef6e57ec8f33ec9333d
William Lin <willlin1996@gmail.com>
2020-01-14 16:40:51 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,24 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board
+ puts "   |   |   "
+ puts "-----------"
+ puts "   |   |   "
+ puts "-----------"
+ puts "   |   |   "
+ end
+ 
+   it 'prints an entire board full of Os' do
+      
+      board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
+      
+      output = capture_puts{ display_board(board) }
+      rows = output.split("\n")
+      
+       expect(rows[0]).to eq(" O | O | O ")
+       expect(rows[1]).to eq("-----------")
+       expect(rows[2]).to eq(" O | O | O ")
+       expect(rows[3]).to eq("-----------")
+       expect(rows[4]).to eq(" O | O | O ")
+       expect(true).to be(true)
+     end
--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -186,5 +186,19 @@ describe "#display_board in 'lib/display_board.rb" do
       # *** Comment the line below by adding a # at the line start ***
       expect(true).to be(true)
     end
+    it 'prints an entire board full of Os' do
+      
+      board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
+      
+      output = capture_puts{ display_board(board) }
+      rows = output.split("\n")
+      
+       expect(rows[0]).to eq(" O | O | O ")
+       expect(rows[1]).to eq("-----------")
+       expect(rows[2]).to eq(" O | O | O ")
+       expect(rows[3]).to eq("-----------")
+       expect(rows[4]).to eq(" O | O | O ")
+       expect(true).to be(true)
+     end
   end
 end
