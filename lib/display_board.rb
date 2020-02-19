cdf4f9fc240cd2459712d3bd374459009a2f4f51
Pratik <pr.tikgohil@gmail.com>
2020-02-19 10:18:16 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board arg
+  puts " #{arg[0]} | #{arg[1]} | #{arg[2]} "
+  puts "-----------"
+  puts " #{arg[3]} | #{arg[4]} | #{arg[5]} "
+  puts "-----------"
+  puts " #{arg[6]} | #{arg[7]} | #{arg[8]} "
+end
+display_board ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
