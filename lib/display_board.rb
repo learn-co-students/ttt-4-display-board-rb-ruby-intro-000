5e5ca5787ab6bd2d308be62b0431d0af13e3bf30
bgb83 <b.graniellobatlle@gmail.com>
2020-03-04 02:19:24 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(play)
+  puts " #{play[0]} | #{play[1]} | #{play[2]} "
+  puts "-----------"
+  puts " #{play[3]} | #{play[4]} | #{play[5]} "
+  puts "-----------"
+  puts " #{play[6]} | #{play[7]} | #{play[8]} "
+end
