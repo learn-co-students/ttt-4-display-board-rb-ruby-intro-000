500fceed7a0d75d6cc667cb0ec94bda97b1a8465
James Jiang <monochrome473@gmail.com>
2020-03-22 01:36:04 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,11 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+board = [" "," "," "," "," "," "," "," "," "]
+def display_board(spot)
+  puts " #{spot[0]} | #{spot[1]} | #{spot[2]} "
+  puts "-----------"
+  puts " #{spot[3]} | #{spot[4]} | #{spot[5]} "
+  puts "-----------"
+  puts " #{spot[6]} | #{spot[7]} | #{spot[8]} "
+  puts "-----------"
+end
+board = ["O"," "," ","O"," "," "," ","X"," "]
+print display_board(board)
