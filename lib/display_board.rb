d47759996a7d5a2a7859e4041028da6b83a25e29
Luis Reyes <luisreyesxv@yahoo.com>
2020-04-25 22:03:40 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+def display_board(position)
+
+ puts " #{position[0]} " +"|"+" #{position[1]} "+"|"+" #{position[2] } "
+ puts "-----------"
+ puts " #{position[3]} " +"|"+" #{position[4]} "+"|"+" #{position[5] } "
+ puts "-----------"
+ puts " #{position[6]} " +"|"+" #{position[7]} "+"|"+" #{position[8] } "
+end
