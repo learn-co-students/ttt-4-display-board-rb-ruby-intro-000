2b3981887bd75704770981fbf3b5f044389a4de4
Mykyta Melnyk <30383@pwsz-kalisz.edu.pl>
2020-05-20 10:15:15 +0000

    Done.

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -165,14 +165,14 @@ describe "#display_board in 'lib/display_board.rb" do
 
       # *** Edit the lines below ***
       # *** Uncomment the lines below ***
-      # expect(rows[0]).to eq("   |   |   ")
-      # expect(rows[1]).to eq("-----------")
-      # expect(rows[2]).to eq("   |   |   ")
-      # expect(rows[3]).to eq("-----------")
-      # expect(rows[4]).to eq("   |   |   ")
+      expect(rows[0]).to eq("   |   |   ")
+      expect(rows[1]).to eq("-----------")
+      expect(rows[2]).to eq("   |   |   ")
+      expect(rows[3]).to eq("-----------")
+      expect(rows[4]).to eq("   |   |   ")
 
       # *** Comment the line below by adding a # at the line start ***
-      expect(true).to be(true)
+      # expect(output).to be(" X | X | X ")
     end
 
     it 'prints an entire board full of Os' do
@@ -184,7 +184,19 @@ describe "#display_board in 'lib/display_board.rb" do
       # example's situation.
 
       # *** Comment the line below by adding a # at the line start ***
-      expect(true).to be(true)
+      
+      board = ["X","O"," ","X","X"," ","O"," "," "]
+
+      output = capture_puts{ display_board(board) }
+      rows = output.split("\n")
+
+      expect(rows[0]).to eq(" X | O |   ")
+      expect(rows[1]).to eq("-----------")
+      expect(rows[2]).to eq(" X | X |   ")
+      expect(rows[3]).to eq("-----------")
+      expect(rows[4]).to eq(" O |   |   ")
+      
+      # expect(true).to be(true)
     end
   end
 end
