d39d862b280620ef4b15588b3590f8f3b81639a8
Brittany Johnson <bj20@rice.edu>
2020-10-06 20:42:45 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+$pipe = "| "
+$horizontal_line = "-----------"
+
+def display_board(board)
+  puts " #{board[0]} " + $pipe + "#{board[1]} " + $pipe + "#{board[2]} "
+  puts $horizontal_line
+  puts " #{board[3]} " + $pipe + "#{board[4]} " + $pipe + "#{board[5]} "
+  puts $horizontal_line
+  puts " #{board[6]} " + $pipe + "#{board[7]} " + $pipe + "#{board[8]} "
+end
