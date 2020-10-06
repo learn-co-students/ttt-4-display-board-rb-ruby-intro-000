45d030f0bd0727966bda51a9f001f972b6956ee5
industrious-chip-8083 <ufumakanneka@gmail.com>
2020-10-06 20:51:46 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,22 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
+board = [" "," "," "," "," "," "," "," "," "]
+board1 = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+board2 = ["O"," "," "," "," "," "," "," "," "]
+board3 = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
+board4 = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+board5 = [" ", " ", " ", " ", " ", " ", "O", "O", "O"]
+board6 = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
+board7 = [" ", " ", "O", " ", "O", " ", "O", " ", " "]
+board8 = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
+board9 = ["X", "X", "X", "X", "X", "X", "X", "X", "X"]
+
