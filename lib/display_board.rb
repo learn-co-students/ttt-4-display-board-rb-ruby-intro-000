96249c19bca97ddc5eda01e9607723e0f1c89dba
Chloe Atwood <atwoodksw@gmail.com>
2020-10-17 02:30:32 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,13 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+#tic tac toe board display
+
+# define array with nine empty strings
+board = ["   ","   ","   ","   ","   ","   ","   ","   ","   "]
+# define method with arguement
+def display_board(board)
+  puts " #{board[0]} | #{board [1]} | #{board[2]} " # puts row 1 of board
+  puts "-----------" # puts border of board
+  puts " #{board[3]} | #{board[4]} | #{board[5]} " # puts row 2 of board
+  puts "-----------" # puts border of board border
+  puts " #{board[6]} | #{board[7]} | #{board[8]} " # puts row 3 of board
+# end definition of method ln. six
+end
