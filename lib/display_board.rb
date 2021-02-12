77186bd1f87fa485d00c30cab01714b17b1a7396
Kathleen Tensfeldt <kmtensfeldt@gmail.com>
2021-02-12 21:31:02 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,12 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-separate = '-----------'
-
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts separate
+  puts '-----------'
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts separate
+  puts '-----------'
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
