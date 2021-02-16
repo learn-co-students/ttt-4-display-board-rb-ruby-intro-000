5f1d97573344853db44438253276250f0ee96c27
Tedi <tmlowney1@gmail.com>
2021-02-16 18:09:24 +0000

    Done.

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -145,7 +145,7 @@ describe "#display_board in 'lib/display_board.rb" do
 
       # Define the board with values that should create the desired output
       # *** Edit the line below ***
-      board = [" ", " ", " ", " ", " ", " ", " ", " ", " "] # This is not correct
+      board = ["X", "X", "X", "X", "X", "X", "X", "X", "X"] # This is not correct
 
       # Don't touch the following lines.
       output = capture_puts{ display_board(board) } if defined?(display_board)
@@ -165,17 +165,42 @@ describe "#display_board in 'lib/display_board.rb" do
 
       # *** Edit the lines below ***
       # *** Uncomment the lines below ***
-      # expect(rows[0]).to eq("   |   |   ")
-      # expect(rows[1]).to eq("-----------")
-      # expect(rows[2]).to eq("   |   |   ")
-      # expect(rows[3]).to eq("-----------")
-      # expect(rows[4]).to eq("   |   |   ")
+       expect(rows[0]).to eq(" X | X | X ")
+       expect(rows[1]).to eq("-----------")
+       expect(rows[2]).to eq(" X | X | X ")
+       expect(rows[3]).to eq("-----------")
+       expect(rows[4]).to eq(" X | X | X ")
 
       # *** Comment the line below by adding a # at the line start ***
-      expect(true).to be(true)
+      #expect(true).to be(true)
     end
 
     it 'prints an entire board full of Os' do
+        # Should you want to write your own test for this situation,
+        # read the following code and comments.
+
+        # Can you copy the syntax of the tests above to write a test for a board
+        # entirely filled with Xs?"
+
+        # Define the board with values that should create the desired output
+        # *** Edit the line below ***
+        board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"] # This is not correct
+
+        # Don't touch the following lines.
+        output = capture_puts{ display_board(board) } if defined?(display_board)
+        rows = output.split("\n")
+
+        # *** Edit the lines below ***
+        # *** Uncomment the lines below ***
+         expect(rows[0]).to eq(" O | O | O ")
+         expect(rows[1]).to eq("-----------")
+         expect(rows[2]).to eq(" O | O | O ")
+         expect(rows[3]).to eq("-----------")
+         expect(rows[4]).to eq(" O | O | O ")
+
+        # *** Comment the line below by adding a # at the line start ***
+        #expect(true).to be(true)
+      
       # Can you copy the syntax of the tests above to write a test for a board
       # entirely filled with Os?
 
@@ -184,7 +209,7 @@ describe "#display_board in 'lib/display_board.rb" do
       # example's situation.
 
       # *** Comment the line below by adding a # at the line start ***
-      expect(true).to be(true)
+      #expect(true).to be(true)
     end
   end
 end
