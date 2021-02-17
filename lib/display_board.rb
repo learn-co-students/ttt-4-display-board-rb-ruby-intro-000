68f45c389a3efb0b6228ccb174fc406171e133d9
Luke Rollins <ldrollins@gmail.com>
2021-02-17 18:17:11 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,34 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+display_board(board)
+
+board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
+display_board(board)
+
+board = ["O", " ", " ", " ", " ", " ", " ", " ", " "]
+display_board(board)
+
+board = ["O", " ", " ", " ", "X", " ", " ", " ", " "]
+display_board(board)
+
+board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
+display_board(board)
+
+board = [" ", " ", " ", " ", " ", " ", "O", "O", "O"]
+display_board(board)
+
+board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
+display_board(board)
+
+board = [" ", " ", "O", " ", "O", " ", "O", " ", " "]
+display_board(board)
+
+board = [" ", "O", " ", "X", "X", "O", " ", " ", " "]
+display_board(board)
